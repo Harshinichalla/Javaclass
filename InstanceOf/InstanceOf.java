937bae9686fe1bc6e129b962626a8cad5157e056
Harshinichalla <charshini2003@gmail.com>
2025-03-21 15:24:18 +0530

    Add files via upload

---
+++ InstanceOf/InstanceOf.java
@@ -0,0 +1,13 @@
+package Samplee;
+
+class Animal{     //parent
+	
+}
+public class InstanceOf {
+
+	public static void main(String[] args) {
+		Animal a=new Animal();
+		System.out.println(a instanceof Animal);
+
+	}
+}
