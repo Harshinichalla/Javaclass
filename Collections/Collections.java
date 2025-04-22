d6cf1163118fcd6e51ad54704540e7d055054b01
Harshinichalla <charshini2003@gmail.com>
2025-04-22 22:35:00 +0530

    Add files via upload

---
+++ Collections/Collections.java
@@ -0,0 +1,16 @@
+//CREATING AN EMPTYLIST
+package Samplee.Interfaces;
+
+import java.util.List;
+import java.util.ArrayList;     //homogeneous collection of elements
+public class Collections {
+
+	public static void main(String[] args) {
+		List<Integer> numbers=new ArrayList<> ();     //syntax for creating empty list
+        System.out.println("My numbers list:"+numbers);
+	}
+
+}
+
+
+//set,list,arrays,maps,linked-list-------->collections,non-primitive data-types
